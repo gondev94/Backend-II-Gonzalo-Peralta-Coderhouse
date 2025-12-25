38d388e4e8a95b3295546db0ee236b8be03ad6d0
Gonzalo Peralta <gonnzaperalta94@gmail.com>
2025-12-25 16:28:30 +0100

    feat/usersRouter/mongoconnect/metodos/base_de_datos

--- src/database/mongooConect.js
+++ src/database/mongooConect.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 
 export const mongoConnect = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:2017/integrative_activity")
-        console.log("Connected to MongoDB")
+        await mongoose.connect("mongodb://localhost:27017/integrative_activity")
+        
     } catch (error) {
         console.log(error)
         throw new Error("Error connecting to MongoDB")
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -4,40 +4,47 @@ const router = Router();
 
 router.get("/", async (req, res, next) => {
     try {
-        const users = await UserModel.find()
-        res.status(200).json(users)
+        const users = await UserModel.find();
+        res.status(200).json(users);
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
-})
+});
 
-
-router.post("/", async (req, res, next) => {
+router.post("/create", async (req, res, next) => {
+    const { first_name, last_name, email, role, password } = req.body;
     try {
-        const user = await UserModel.create(req.body)
-        res.status(201).json(user)
+        const user = await UserModel.create({
+            first_name,
+            last_name,
+            email,
+            role,
+            password,
+        });
+        res.status(201).json(user);
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
-})
+});
 
-router.put("/:id", async (req, res, next) => {
+router.put("/update/:id", async (req, res, next) => {
+    const { first_name, last_name, password } = req.body;
     try {
-        const user = await UserModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        res.status(200).json(user)
+        const user = await UserModel.findByIdAndUpdate(req.params.id,req.body,{ new: true });
+        res.status(200).json(user);
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
-})
+});
 
-
-router.delete("/:id", async (req, res, next) => {
+router.delete("/delete/:id", async (req, res, next) => {
+    const { id } = req.params;
     try {
-        await UserModel.findByIdAndDelete(req.params.id)
-        res.status(200).json({ message: "User deleted successfully" })
+        await UserModel.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "User deleted successfully" });
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
-})
+});
 
 export default router;
